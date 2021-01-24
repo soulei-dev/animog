e0fe007473671e9fe06dc2da4ed6afffde37e889
Souleimane <soulei.dev@gmail.com>
2021-01-24 11:39:45 +0100

    multi-page webpack configuration

---
+++ src/form/form.js
@@ -0,0 +1,3 @@
+import './form.scss';
+
+console.log('form ok');
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: {
-        main: path.join(__dirname, "src/index.js")
+        main: path.join(__dirname, "src/index.js"),
+        form: path.join(__dirname, "src/form/form.js")
     },
     output: {
         path: path.join(__dirname, "dist"),
@@ -24,7 +25,14 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.join(__dirname, "./src/index.html")
+            filename: 'index.html',
+            template: path.join(__dirname, "./src/index.html"),
+            chunks: ["main"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'form.html',
+            template: path.join(__dirname, "./src/form/form.html"),
+            chunks: ["form"]
         })
     ],
     stats: "minimal",
