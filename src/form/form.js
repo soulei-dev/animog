c7201fd6e977b5e0b87f4268b92de4180b75f333
Souleimane <soulei.dev@gmail.com>
2021-01-25 18:56:37 +0100

    optimal style architecture

--- src/form/form.js
+++ src/form/form.js
@@ -1,3 +1,3 @@
-import './form.scss';
+import '../assets/styles/styles.scss';
 
 console.log('form ok');
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './assets/styles/styles.scss';
 import './index.scss';
 
 console.log('index ok');
